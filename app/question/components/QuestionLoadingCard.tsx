1637942c2e2d1cf40122bc3bee859ebad16fcc6f
Shrinidhi Upadhyaya <shrinidhiupadhyaya1195@gmail.com>
2025-02-08 17:52:29 +0100

    feat: loading skeleton card component implementation

---
+++ app/question/components/QuestionLoadingCard.tsx
@@ -0,0 +1,49 @@
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+
+const QuestionLoadingCard = () => {
+  return (
+    <Card className="w-full">
+      <CardHeaderSkeleton />
+      <CardContentSkeleton />
+      <CardFooterSkeleton />
+    </Card>
+  );
+};
+
+const CardHeaderSkeleton = () => {
+  return (
+    <CardHeader>
+      <Skeleton className="w-full h-10" />
+    </CardHeader>
+  );
+};
+
+const CardContentSkeleton = () => {
+  return (
+    <CardContent>
+      <div className="flex flex-col items-center justify-center space-y-2">
+        {Array.from({ length: 4 }).map((_, index) => (
+          <Skeleton key={index} className="w-full h-10" />
+        ))}
+      </div>
+    </CardContent>
+  );
+};
+
+const CardFooterSkeleton = () => {
+  return (
+    <CardFooter className="justify-between">
+      {Array.from({ length: 2 }).map((_, index) => (
+        <Skeleton key={index} className="w-24 h-10" />
+      ))}
+    </CardFooter>
+  );
+};
+
+export default QuestionLoadingCard;
